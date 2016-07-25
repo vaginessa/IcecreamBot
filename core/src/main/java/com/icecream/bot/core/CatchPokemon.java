7839b6f593f2f8dd51e0888a56e4806fae89e8ca
Pedro Díaz <igoticecream@gmail.com>
2016-07-25 18:49:04 -0400

    Minor changes

--- core/src/main/java/com/icecream/bot/core/CatchPokemon.java
+++ core/src/main/java/com/icecream/bot/core/CatchPokemon.java
@@ -4,6 +4,7 @@ import com.pokegoapi.api.inventory.Pokeball;
 import com.pokegoapi.api.map.pokemon.CatchResult;
 import com.pokegoapi.api.map.pokemon.CatchablePokemon;
 import com.pokegoapi.api.map.pokemon.EncounterResult;
+
 import rx.Observable;
 
 @SuppressWarnings({"unused", "FieldCanBeLocal", "WeakerAccess"})
@@ -19,6 +20,33 @@ public final class CatchPokemon {
                         .filter(EncounterResult::wasSuccessful)
                         .map(data -> pokemon)
                 )
-                .flatMap(pokemon -> Observable.fromCallable(() -> pokemon.catchPokemon(Pokeball.POKEBALL)));
+                .flatMap(pokemon ->
+                        Observable
+                                .fromCallable(() -> pokemon.catchPokemon(Pokeball.POKEBALL))
+                                .flatMap(result -> {
+                                    switch (result.getStatus()) {
+                                        case CATCH_SUCCESS:
+                                            return Observable.just(result);
+                                        case CATCH_FLEE:
+                                            return Observable.error(new CatchFleeException());
+                                        case CATCH_ESCAPE:
+                                        case CATCH_MISSED:
+                                            return Observable.error(new CatchEscapeException());
+                                        default:
+                                            return Observable.empty();
+                                    }
+                                })
+                                .retryWhen(errors -> errors.flatMap(error -> {
+                                    if (error instanceof CatchFleeException) {
+                                        return Observable.empty();
+                                    }
+
+                                    if (error instanceof CatchEscapeException) {
+                                        return Observable.just(null);
+                                    }
+
+                                    return Observable.error(error);
+                                }))
+                );
     }
 }
--- core/src/test/java/com/icecream/bot/core/CatchPokemonTest.java
+++ core/src/test/java/com/icecream/bot/core/CatchPokemonTest.java
@@ -12,10 +12,16 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import POGOProtos.Networking.Responses.CatchPokemonResponseOuterClass.CatchPokemonResponse.CatchStatus;
 import POGOProtos.Networking.Responses.EncounterResponseOuterClass.EncounterResponse.Status;
 import rx.Observable;
 import rx.observers.TestSubscriber;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
@@ -32,7 +38,9 @@ public class CatchPokemonTest {
     @Mock
     private CatchablePokemon mPokemon;
     @Mock
-    private EncounterResult mResult;
+    private EncounterResult mEncounterResult;
+    @Mock
+    private CatchResult mCatchResult;
 
     private TestSubscriber<CatchResult> mSubscriber;
 
@@ -53,24 +61,29 @@ public class CatchPokemonTest {
         Observable<CatchablePokemon> observable = Observable.just(mPokemon);
 
         //When
-        doReturn(true).when(mResult).wasSuccessful();
-        doReturn(mResult).when(mPokemon).encounterPokemon();
+        doReturn(true).when(mEncounterResult).wasSuccessful();
+        doReturn(mEncounterResult).when(mPokemon).encounterPokemon();
+
+        doReturn(CatchStatus.CATCH_SUCCESS).when(mCatchResult).getStatus();
+        doReturn(mCatchResult).when(mPokemon).catchPokemon(any());
 
         //Then
         observable
                 .compose(CatchPokemon.catchIt())
                 .subscribe(mSubscriber);
 
-        verify(mResult, times(1)).wasSuccessful();
+        verify(mEncounterResult, times(1)).wasSuccessful();
         verify(mPokemon, times(1)).encounterPokemon();
         verify(mPokemon, times(1)).catchPokemon(any());
 
-        verifyNoMoreInteractions(mResult);
+        verifyNoMoreInteractions(mEncounterResult);
         verifyNoMoreInteractions(mPokemon);
 
         mSubscriber.assertCompleted();
         mSubscriber.assertNoErrors();
-        mSubscriber.assertValueCount(1);
+
+        assertThat("Source emitted unexpected number of items", mSubscriber.getValueCount(), is(1));
+        assertThat("Source emitted unexpected items", mSubscriber.getOnNextEvents(), containsInAnyOrder(mCatchResult));
     }
 
     @Test
@@ -79,26 +92,28 @@ public class CatchPokemonTest {
         Observable<CatchablePokemon> observable = Observable.just(mPokemon);
 
         //When
-        doReturn(Status.POKEMON_INVENTORY_FULL).when(mResult).getStatus();
-        doReturn(false).when(mResult).wasSuccessful();
+        doReturn(Status.POKEMON_INVENTORY_FULL).when(mEncounterResult).getStatus();
+        doReturn(false).when(mEncounterResult).wasSuccessful();
 
-        doReturn(mResult).when(mPokemon).encounterPokemon();
+        doReturn(mEncounterResult).when(mPokemon).encounterPokemon();
 
         //Then
         observable
                 .compose(CatchPokemon.catchIt())
                 .subscribe(mSubscriber);
 
-        verify(mResult, times(1)).wasSuccessful();
+        verify(mEncounterResult, times(1)).wasSuccessful();
         verify(mPokemon, times(1)).encounterPokemon();
         verify(mPokemon, never()).catchPokemon(any());
 
-        verifyNoMoreInteractions(mResult);
+        verifyNoMoreInteractions(mEncounterResult);
         verifyNoMoreInteractions(mPokemon);
 
         mSubscriber.assertCompleted();
         mSubscriber.assertNoErrors();
-        mSubscriber.assertValueCount(0);
+
+        assertThat("Source emitted unexpected number of items", mSubscriber.getValueCount(), is(0));
+        assertThat("Source emitted unexpected items", mSubscriber.getOnNextEvents(), empty());
     }
 
     @Test
@@ -115,10 +130,13 @@ public class CatchPokemonTest {
                 .subscribe(mSubscriber);
 
         verify(mPokemon, only()).encounterPokemon();
-        verifyZeroInteractions(mResult);
+        verifyZeroInteractions(mEncounterResult);
 
         mSubscriber.assertNotCompleted();
         mSubscriber.assertError(LoginFailedException.class);
         mSubscriber.assertValueCount(0);
+
+        assertThat("Source emitted unexpected number of items", mSubscriber.getValueCount(), is(0));
+        assertThat("Source emitted unexpected items", mSubscriber.getOnErrorEvents(), not(empty()));
     }
 }
